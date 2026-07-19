76e686c9c19abcd4f544c43d2da6a2cde39f4944
namyong <chonamyong1988@gmail.com>
2026-07-20 00:23:00 +0900

    수도는 제외

--- src/map/atcommand.cpp
+++ src/map/atcommand.cpp
@@ -782,6 +782,7 @@ ACMD_FUNC(who) {
 	 */
 	int32 display_type = 1;
 	int32 map_id = -1;
+	int32 excluded_map_id = -1;
 
 	nullpo_retr(-1, sd);
 
@@ -798,11 +799,20 @@ ACMD_FUNC(who) {
 	else if (strstr(command, "3") != nullptr)
 		display_type = 3;
 
+	const char* command_name = command;
+	if( *command_name == atcommand_symbol || *command_name == charcommand_symbol )
+		command_name++;
+	if( strcmpi(command_name, "who3") == 0 )
+		excluded_map_id = map_mapname2mapid("lasagna");
+
 	level = pc_get_group_level(sd);
 	StringBuf_Init(&buf);
 
 	iter = mapit_getallusers();
 	for (pl_sd = (TBL_PC*)mapit_first(iter); mapit_exists(iter); pl_sd = (TBL_PC*)mapit_next(iter))	{
+		if( excluded_map_id >= 0 && pl_sd->m == excluded_map_id )
+			continue;
+
 		if (!((pc_has_permission(pl_sd, PC_PERM_HIDE_SESSION) || pc_isinvisible(pl_sd)) && pc_get_group_level(pl_sd) > level)) { // you can look only lower or same level
 			if (stristr(pl_sd->status.name, player_name) == nullptr // search with no case sensitive
 				|| (map_id >= 0 && pl_sd->m != map_id))
